1d7a4753982be821d3615e2fc08966a271a3ea1b
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2022-12-14 22:50:57 +0530

    initial commit

---
+++ routes/questionRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const questionController = require('./../controllers/questionController');
+const authController = require('./../controllers/authController');
+const commentRouter = require('./../routes/commentRoutes');
+
+
+const router = express.Router();
+
+router.use('/:questionid/comment', commentRouter);
+
+
+//this line is to use /api/v1/question/ in get and post
+router
+    .route('/')
+    .get(questionController.getAllQuestion)
+    .post(authController.protect, authController.restrictTo('admin'), questionController.createQuestion);
+
+//this line is to use /api/v1/question/solved in get and post
+router
+    .route('/solved')
+    .get(authController.protect, questionController.solvedQuestion);
+
+//this line is to use /api/v1/question/unsolved in get and post
+router
+    .route('/unsolved')
+    .get(authController.protect, questionController.unsolvedQuestion);
+
+
+// this line is to use /api/v1/question/:id in get, patch and delete
+router
+    .route('/:id')
+    .get(authController.protect, questionController.getOneQuestion)
+    .patch(authController.protect, authController.restrictTo('admin'), questionController.updateQuestion)
+    .delete(authController.protect, authController.restrictTo('admin'), questionController.deleteQuestion);
+
+router
+    .route('/:id/submit')
+    .patch(authController.protect, questionController.submitQuestion);
+
+
+module.exports = router;
