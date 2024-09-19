626cd2d0bc8d12c249526dbaa78a5c57e98cadb1
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2024-09-19 21:12:11 +0530

    docker file added

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' })
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
-const DB = process.env.DATABASE
+const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
-}).then(con =>
-    console.log('DB connection successful...')
-);
+    useFindAndModify: false,
+  })
+  .then((con) => console.log("DB connection successful..."));
 
 //this line shows the environment in which our project is running by default development
 // console.log(app.get('env'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App Runing on port ${port}...`);
+  console.log(`App Runing on port ${port}...`);
 });
-
